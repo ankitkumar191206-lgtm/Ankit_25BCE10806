803a09aec71f38a06889ecad1513633138eaeaa2
Ankit Kumar <your-ankitkumar191206@gmail.com>
2026-09-04 11:11:30 +0530

    init: project setup

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+server.listen(5000, () => console.log("Server running on 5000"));
